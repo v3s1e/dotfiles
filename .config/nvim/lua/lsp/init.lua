4f3440ca82979ba79d95f011d58f77326803dff1
Veselin Ivanov <v3s1ez@gmail.com>
2021-02-21 21:16:00 +0200

    chore(vim): renaming functions

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,51 +1,47 @@
 local lsp = require('lspconfig')
 
-local on_attach = function(client, bufnr)
-    local function nvim_buf_set_keymap(...)
+local on_attach = function(_, bufnr)
+    local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
-    local function nvim_buf_set_option(...)
+    local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
 
-    nvim_buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     local opts = {noremap = true, silent = true}
-    nvim_buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    nvim_buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    nvim_buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', '<C-k>',
-                        '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>wa',
-                        '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>wr',
-                        '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', '<leader>wl',
-                        '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', '<leader>D',
-                        '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>e',
-                        '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                        opts)
-    nvim_buf_set_keymap('n', '[d',
-                        '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    nvim_buf_set_keymap('n', ']d',
-                        '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>q',
-                        '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+                   opts)
+    buf_set_keymap('n', '<leader>wa',
+                   '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr',
+                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl',
+                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                   opts)
+    buf_set_keymap('n', '<leader>D',
+                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>e',
+                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                   opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+                   opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+                   opts)
+    buf_set_keymap('n', '<leader>q',
+                   '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
-    nvim_buf_set_keymap('n', '<leader>f',
-                        '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    nvim_buf_set_keymap('n', '<leader>rf',
-                        '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
+    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>',
+                   opts)
+    buf_set_keymap('n', '<leader>rf',
+                   '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
     vim.api.nvim_command(
         'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)')
 
