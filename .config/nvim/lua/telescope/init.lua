e8cdef0b101e49e19c2ed4949fab40496f891d33
Veselin Ivanov <v3s1ez@gmail.com>
2021-03-01 00:05:42 +0200

    chore(vim): telescope keymaps again

--- .config/nvim/lua/telescope/init.lua
+++ .config/nvim/lua/telescope/init.lua
@@ -7,8 +7,8 @@ set_keymap('n', '<leader>gg', '<cmd>Telescope live_grep<CR>', opts)
 set_keymap('n', '<leader>gb', '<cmd>Telescope buffers<CR>', opts)
 set_keymap('n', '<leader>gh', '<cmd>Telescope help_tags<CR>', opts)
 
-set_keymap('n', '<leader>gd', '<cmd>Telescope lsp_document_symbols<CR>', opts)
-set_keymap('n', '<leader>gw', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
+set_keymap('n', '<leader>d', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+set_keymap('n', '<leader>w', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
 
 set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
 set_keymap('n', '<leader>ga', '<cmd>Telescope lsp_code_actions<CR>', opts)
