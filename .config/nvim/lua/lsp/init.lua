d135343c485e64c228d3263b9a095844db9b35b0
Veselin Ivanov <v3s1ez@gmail.com>
2021-03-07 17:39:31 +0100

    chore(vim): keymaps

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -25,8 +25,6 @@ local on_attach = function(client, bufnr)
                    opts)
     buf_set_keymap('n', '<leader>D',
                    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>q',
-                   '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>',
                    opts)
     buf_set_keymap('n', '<leader>rf',
--- .config/nvim/lua/telescope/init.lua
+++ .config/nvim/lua/telescope/init.lua
@@ -7,10 +7,12 @@ set_keymap('n', '<leader>gg', '<cmd>Telescope live_grep<CR>', opts)
 set_keymap('n', '<leader>gb', '<cmd>Telescope buffers<CR>', opts)
 set_keymap('n', '<leader>gh', '<cmd>Telescope help_tags<CR>', opts)
 
-set_keymap('n', '<leader>ds', '<cmd>Telescope lsp_document_symbols<CR>', opts)
-set_keymap('n', '<leader>ws', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
+set_keymap('n', '<leader>sd', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+set_keymap('n', '<leader>qd', '<cmd>Telescope lsp_document_diagnostics<CR>',
+           opts)
 
-set_keymap('n', '<leader>dd', '<cmd>Telescope lsp_document_diagnostics<CR>', opts)
-set_keymap('n', '<leader>wd', '<cmd>Telescope lsp_workspace_diagnostics<CR>', opts)
+set_keymap('n', '<leader>sw', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
+set_keymap('n', '<leader>qw', '<cmd>Telescope lsp_workspace_diagnostics<CR>',
+           opts)
 
 set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
